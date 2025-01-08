06390c0eb7f3b736700f0b430a3e835f8688aa8a
Nhan Nguyen <hoainhaannguyen@gmail.com>
2025-01-08 10:59:09 +0700

    Run with env files

--- src/routes/v1/v1.mjs
+++ src/routes/v1/v1.mjs
@@ -1,8 +1,8 @@
 import express from 'express';
 import swaggerJsdoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
-import { BullMQConfigs } from '../../app.config.mjs';
-import * as bullmq from '../../services/bullmq/bullmq.mjs';
+// import { BullMQConfigs } from '../../app.config.mjs';
+// import * as bullmq from '../../services/bullmq/bullmq.mjs';
 import { AuthRouter } from './auth/auth.controller.mjs';
 import { PingRouter } from './ping/ping.controller.mjs';
 import { UsersRouter } from './users/users.controller.mjs';
@@ -33,8 +33,9 @@ router
   .use('/ping', PingRouter)
   .use('/auth', AuthRouter)
   .use('/users', UsersRouter)
-  .use(BullMQConfigs.BULLMQ_ADMIN_PATH, bullmq.serverAdapter.getRouter())
+  // .use(BullMQConfigs.BULLMQ_ADMIN_PATH, bullmq.serverAdapter.getRouter())
   .use('/api-docs', swaggerUi.serve)
   .get('/api-docs', swaggerUi.setup(swaggerJsdoc(swaggerJsdocOptions)));
 
 export { router as V1Router };
+
